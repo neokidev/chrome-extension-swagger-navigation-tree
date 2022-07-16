4fed4c87924eeef7399377836beed9130fd554e5
ot07 <ot07neoki@gmail.com>
2022-07-17 00:03:07 +0900

    🐛 Support for docs different from Petstore demo on how to get elements

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,18 @@ import "./index.css";
 
 const NAVIGATION_TREE_WIDTH = "260px";
 
-const onLoadedSwaggerTagSection = (
-  callback: (swaggerUiMain: HTMLElement) => void
+const onLoadedSwaggerTagSectionsAndModelContainers = (
+  callback: (
+    tagSections: HTMLCollectionOf<Element>,
+    modelContainers: HTMLCollectionOf<Element>
+  ) => void
 ) => {
   const intervalID = setInterval(() => {
-    const tagSection = document.querySelector(
-      "#swagger-ui > section > div.swagger-ui > div:nth-child(2) > div:nth-child(4) > section > div"
-    ) as HTMLElement;
+    const tagSections = document.getElementsByClassName("opblock-tag-section");
+    const modelContainers = document.getElementsByClassName("model-container");
 
-    if (tagSection) {
-      callback(tagSection);
+    if (tagSections.length && modelContainers.length) {
+      callback(tagSections, modelContainers);
       clearInterval(intervalID);
     }
   }, 1000);
@@ -57,47 +59,50 @@ const renderNavigationTree = (items: NavigationTreeItem[]) => {
 
 const swaggerUiRoot = document.getElementById("swagger-ui");
 if (swaggerUiRoot) {
-  onLoadedSwaggerTagSection((tagSection) => {
-    const tags = tagSection.children;
-    const navigationTagItems = Array.from(tags).map((tag) => {
-      const tagName = tag.firstElementChild?.firstElementChild?.getAttribute(
-        "data-tag"
-      ) as string;
+  onLoadedSwaggerTagSectionsAndModelContainers(
+    (tagSections, modelContainers) => {
+      const navigationTagItems = Array.from(tagSections).map((tagSection) => {
+        const tagName = tagSection.firstElementChild?.getAttribute(
+          "data-tag"
+        ) as string;
 
-      const tagContents = tag.getElementsByClassName("operation-tag-content")[0]
-        .children;
+        const tagContents = tagSection.getElementsByClassName(
+          "operation-tag-content"
+        )[0].children;
 
-      const navigationTreeTagContents = Array.from(tagContents).map(
-        (tagContent) => {
-          const method = (
-            tagContent.getElementsByClassName(
-              "opblock-summary-method"
-            )[0] as HTMLElement
-          ).innerText as Method;
+        const navigationTreeTagContents = Array.from(tagContents).map(
+          (tagContent) => {
+            const method = (
+              tagContent.getElementsByClassName(
+                "opblock-summary-method"
+              )[0] as HTMLElement
+            ).innerText as Method;
 
-          const description = (
-            tagContent.getElementsByClassName(
-              "opblock-summary-description"
-            )[0] as HTMLElement
-          ).innerText;
+            const description = (
+              tagContent.getElementsByClassName(
+                "opblock-summary-description"
+              )[0] as HTMLElement
+            ).innerText;
 
-          const deprecated =
-            tagContent.querySelector("#operations-pet-findPetsByTags") !== null;
+            const deprecated =
+              tagContent.querySelector("#operations-pet-findPetsByTags") !==
+              null;
 
-          return {
-            method,
-            description,
-            deprecated,
-          };
-        }
-      );
+            return {
+              method,
+              description,
+              deprecated,
+            };
+          }
+        );
 
-      return {
-        tagName,
-        tagContents: navigationTreeTagContents,
-      };
-    });
+        return {
+          tagName,
+          tagContents: navigationTreeTagContents,
+        };
+      });
 
-    renderNavigationTree(navigationTagItems);
-  });
+      renderNavigationTree(navigationTagItems);
+    }
+  );
 }
